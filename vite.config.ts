c63cde05f8086604b478b064178c21ab9f3138d4
Matías Gutiérrez <mattygh2017@gmail.com>
2025-10-30 00:50:15 -0300

    Eliminación de archivo y configuración innecesaria para los tests

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
     include: ['src/**/*.test.{ts,tsx}'],
     exclude: ['proyecto-guia/**', 'dist/**', 'node_modules/**']
   },
