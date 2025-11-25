68fe51d26ce98bb31ce581f4b1abb5924363d930
Matías Gutiérrez <mattygh2017@gmail.com>
2025-11-25 02:59:49 -0300

    feat: la aplicación React incializa con proveedores de enrutamiento y contexto, y agregar el estilo CartItemRow.

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,6 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import './assets/styles/bootstrap-overrides.css';
 import './assets/styles/main.css';
-import './assets/styles/layout.css';
-import './assets/styles/components.css';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
